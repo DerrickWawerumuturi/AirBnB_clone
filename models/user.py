ed83e21640042cdb8ec53324ca9abe88f56c24c2
esther-nyokabi <enyokabimuturi@gmail.com>
2024-03-12 13:50:51 +0300

    user

--- models/user.py
+++ models/user.py
@@ -8,9 +8,8 @@ class User(BaseModel):
         attributes: email, password, first_name, last_name
     """
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
         
