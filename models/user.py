853976bea4957129135a5a244ab4c729497cd072
esther-nyokabi <enyokabimuturi@gmail.com>
2024-03-12 14:59:15 +0300

    test cases

--- models/user.py
+++ models/user.py
@@ -8,6 +8,7 @@ class User(BaseModel):
         attributes: email, password, first_name, last_name
     """
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         email = ""
         password = ""
         first_name = ""
